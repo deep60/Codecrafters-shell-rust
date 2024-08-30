8930e717d757ed5c3664de31b95154e6fe6d8e0a
P Deepanshu <97436890+deep60@users.noreply.github.com>
2024-08-30 23:36:00 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,22 @@ use std::io::{self, Write};
 fn main() {
     let stdin = io::stdin();
     let mut input = String::new();
-    print!("$ ");
-    io::stdout().flush().unwrap();
-    while stdin.read_line(&mut input).is_ok() {
-        println!("{}: command not found", input.strip_suffix("\n").unwrap());
-        input.clear();
+
+    loop  {
         print!("$ ");
         io::stdout().flush().unwrap();
+        // println!("{}: command not found", input.strip_suffix("\n").unwrap());
+        input.clear();
+        stdin.read_line(&mut input).unwrap();
+        let command = input.trim();
+
+        if command.is_empty() {
+            continue;
+        } else if command == "exit 0" {
+            break;
+        } else {
+            println!("{}: command not found", command);
+        }
+        
     }
 }
